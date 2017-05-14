55fe66d4afc463bb421bc778fc0f1b20f746cc66
Øyvind A. Holm <sunny@sunbase.org>
2017-05-14 23:02:30 +0200

    filesynced --init, lag synced.sql
    Den brukes av et program (sunny256/utils.git/filesynced) som lagrer
    sync-info for filer som kopieres fra andre steder. Er ikke noe som folk
    flest trenger å tenke på.
    
    04dd79ee-38e9-11e7-9e93-f74d993421b0
    
    Signed-off-by: Øyvind A. Holm <oyvind@piratpartiet.no>

---
+++ synced.sql
@@ -0,0 +1,35 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE synced (
+  file TEXT
+    CONSTRAINT synced_file_length
+      CHECK (length(file) > 0)
+    UNIQUE
+    NOT NULL
+  ,
+  orig TEXT
+  ,
+  rev TEXT
+    CONSTRAINT synced_rev_length
+      CHECK (length(rev) = 40 OR rev = '')
+  ,
+  date TEXT
+    CONSTRAINT synced_date_length
+      CHECK (date IS NULL OR length(date) = 19)
+    CONSTRAINT synced_date_valid
+      CHECK (date IS NULL OR datetime(date) IS NOT NULL)
+);
+CREATE TABLE todo (
+  file TEXT
+    CONSTRAINT todo_file_length
+      CHECK(length(file) > 0)
+    UNIQUE
+    NOT NULL
+  ,
+  pri INTEGER
+    CONSTRAINT todo_pri_range
+      CHECK(pri BETWEEN 1 AND 5)
+  ,
+  comment TEXT
+);
+COMMIT;
